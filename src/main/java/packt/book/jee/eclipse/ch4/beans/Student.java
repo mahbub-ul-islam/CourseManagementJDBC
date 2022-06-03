db627d4a305b06d32bb72b988c1fdb347221be46
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-06-03 16:39:29 +0600

    Create Student.java

---
+++ src/main/java/packt/book/jee/eclipse/ch4/beans/Student.java
@@ -0,0 +1,16 @@
+package packt.book.jee.eclipse.ch4.beans;
+
+public class Student extends Person {
+	
+	private long enrolledSince;
+
+	public long getEnrolledSince() {
+		return enrolledSince;
+	}
+
+	public void setEnrolledSince(long enrolledSince) {
+		this.enrolledSince = enrolledSince;
+	}
+	
+	
+}
